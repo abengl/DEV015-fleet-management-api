7dcddda05a452aafab4c696d2a1c337d430874a6
Aben <126703861+abengl@users.noreply.github.com>
2024-08-30 14:00:59 -0500

    fix: adding annotations for path variable #21

--- src/main/java/com/fleetmanagement/api_rest/controller/TrajectoryController.java
+++ src/main/java/com/fleetmanagement/api_rest/controller/TrajectoryController.java
@@ -1,6 +1,5 @@
 package com.fleetmanagement.api_rest.controller;
 
-import com.fleetmanagement.api_rest.model.Taxi;
 import com.fleetmanagement.api_rest.model.Trajectory;
 import com.fleetmanagement.api_rest.service.TrajectoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,12 +7,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.sql.Date;
 import java.sql.Timestamp;
 import java.util.List;
 
@@ -40,7 +35,7 @@ public class TrajectoryController {
 
 	@GetMapping("/{taxiId}")
 	public ResponseEntity<List<Trajectory>> getTrajectoriesByIdAndDate(
-			Integer taxiId,
+			@PathVariable Integer taxiId,
 			@RequestParam(name = "date", required = false) Timestamp date,
 			@RequestParam(name = "page", defaultValue = "1") int page,
 			@RequestParam(name = "limit", defaultValue = "100") int limit) {
